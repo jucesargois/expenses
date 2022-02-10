d01dcd6ed77fc3dd510ba35833aed3ad1fea2170
Julio <jucesargois@gmail.com>
2022-02-10 00:47:09 -0300

    Added new function: addTransaction. for insert new items in Transaction list

--- lib/components/transaction_user.dart
+++ lib/components/transaction_user.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:expenses/components/transaction_form.dart';
 import 'package:expenses/components/transaction_list.dart';
 import 'package:flutter/material.dart';
@@ -27,12 +29,24 @@ class _TransactionUserState extends State<TransactionUser> {
     ),
   ];
 
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
-      children:  [
+      children: [
         TransactionList(transactions: _transactions),
-        TransactionForm(),
+        TransactionForm(_addTransaction),
       ],
     );
   }
