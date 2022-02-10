411458c81ee1da01574b55b860bc61652ff7fadb
Julio <jucesargois@gmail.com>
2022-02-10 00:44:33 -0300

    Inserted function : onSubmit for TextButton.

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
 class TransactionForm extends StatelessWidget {
-   TransactionForm({Key? key}) : super(key: key);
-final titleController = TextEditingController();
+  final titleController = TextEditingController();
   final valueController = TextEditingController();
+
+  final Function(String, double) onSubmit;
+
+  TransactionForm(this.onSubmit, {Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -25,8 +28,9 @@ final titleController = TextEditingController();
               children: [
                 TextButton(
                   onPressed: () {
-                    print(titleController.text);
-                    print(valueController.text);
+                    final title = titleController.text;
+                    final value = double.tryParse(valueController.text) ?? 0.0;
+                    onSubmit(title, value);
                   },
                   child: const Text(
                     'Nova Transação',
