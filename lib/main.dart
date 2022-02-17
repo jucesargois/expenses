b803af7ea01723cbc818d014e01add4e2777fa5b
Julio <jucesargois@gmail.com>
2022-02-17 00:28:28 -0300

    Inserted floatingactionbuttons

--- lib/main.dart
+++ lib/main.dart
@@ -21,13 +21,16 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  MyHomePage({Key? key}) : super (key: key);
+  MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Despesas Pessoais'),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -43,6 +46,11 @@ class MyHomePage extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
