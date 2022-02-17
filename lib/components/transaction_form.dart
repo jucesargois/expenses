01947b1431e9291b1196a669b1f412473db32214
Julio <jucesargois@gmail.com>
2022-02-17 00:42:38 -0300

    Converted statelessWidget for statefulWidget

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
-class TransactionForm extends StatelessWidget {
-  final titleController = TextEditingController();
-  final valueController = TextEditingController();
-
+class TransactionForm extends StatefulWidget {
   final Function(String, double) onSubmit;
+
+  TransactionForm(this.onSubmit, {Key? key}) : super(key: key);
+
+  @override
+  State<TransactionForm> createState() => _TransactionFormState();
+}
+
+class _TransactionFormState extends State<TransactionForm> {
+  final titleController = TextEditingController();
+
+  final valueController = TextEditingController();
 
   _submitForm() {
     final title = titleController.text;
@@ -13,10 +21,9 @@ class TransactionForm extends StatelessWidget {
     if (title.isEmpty || value <= 0) {
       return;
     }
-    onSubmit(title, value);
+    widget.onSubmit(title, value);
   }
 
-  TransactionForm(this.onSubmit, {Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Card(
